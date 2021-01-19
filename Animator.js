0f67fff8c7b2233368b5ee9e6d716368157e0384
Walter <wrkagel@uw.edu>
2021-01-19 15:18:18 -0800

    Added JS files for Animator, Scene Manager, enemies, and druid. Added placeholder sprites for enemies and player. Begun coding fly enemy.

---
+++ Animator.js
@@ -0,0 +1,55 @@
+// JavaScript source code
+class Animator {
+    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop, flip) {
+        Object.assign(this, { spritesheet, xStart, yStart, height, width, frameCount, frameDuration, framePadding, reverse, loop, flip});
+
+        this.elapsedTime = 0;
+        this.totalTime = this.frameCount * this.frameDuration;
+
+    };
+
+    drawFrame(tick, ctx, x, y, scale) {
+        this.elapsedTime += tick;
+
+        if (this.isDone()) {
+            if (this.loop) {
+                this.elapsedTime -= this.totalTime;
+            } else {
+                return;
+            }
+        }
+
+        let frame = this.currentFrame();
+        if (this.reverse) frame = this.frameCount - frame - 1;
+
+        if (this.flip) {
+            ctx.save();
+            ctx.scale(-1, 1);
+            x = 0 - x - width * scale;
+        }
+
+        ctx.drawImage(this.spritesheet,
+            this.xStart + frame * (this.width + this.framePadding), this.yStart, //source from sheet
+            this.width, this.height,
+            x, y,
+            this.width * scale,
+            this.height * scale);
+
+        if (this.flip) {
+            ctx.restore();
+        }
+
+/*        if (PARAMS.DEBUG) {
+            ctx.strokeStyle = 'Green';
+            ctx.strokeRect(x, y, this.width * scale, this.height * scale);
+        }*/
+    };
+
+    currentFrame() {
+        return Math.floor(this.elapsedTime / this.frameDuration);
+    };
+
+    isDone() {
+        return (this.elapsedTime >= this.totalTime);
+    };
+};
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var gameEngine = new GameEngine();
 
 var ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./Sprites/TestEnemy.png");
+
 ASSET_MANAGER.downloadAll(function () {
 	var canvas = document.getElementById('gameWorld');
 	var ctx = canvas.getContext('2d');

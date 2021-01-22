511876f82b5905bc1f2bf6276e095f3157a1da9b
Walter <wrkagel@uw.edu>
2021-01-22 10:35:33 -0800

    Fixing name issues.

--- sceneManager.js
+++ sceneManager.js
@@ -9,8 +9,8 @@ class SceneManager {
 	loadTestLevel() {
 		var player = new Druid(this.game, randomInt(800), randomInt(600));
 		this.game.addEntity(player)
-		this.game.addEntity(new fly(this.game, player, randomInt(800), randomInt(600)));
-		this.game.addEntity(new beetle(this.game, 0, this.game.surfaceHeight - 64));
-		this.game.addEntity(new hopper(this.game, player, 100, randomInt(600)))
+		this.game.addEntity(new Fly(this.game, player, randomInt(800), randomInt(600)));
+		this.game.addEntity(new Beetle(this.game, 0, this.game.surfaceHeight - 64));
+		this.game.addEntity(new Hopper(this.game, player, 100, randomInt(600)))
 	}
 }

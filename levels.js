a8b3cee0639acc2501d66ffec72a63fd2763c534
8BitMissingno <lucas.d.m.gomez@gmail.com>
2021-02-10 16:45:44 -0800

    Made some changes to the level.

--- levels.js
+++ levels.js
@@ -1,15 +1,13 @@
 var levelOne = {
 	ground: [
-		{ x: -7, y: -1, width: 4, height: 3 },
-
 		// Entrance
 		{ x: -1, y: -1, width: 3, height: 3 },
 		{ x: -10, y: 6, width: 22, height: 22 },
-		{ x: 3, y: -1, width: 32 },
-		{ x: 35, y: -1, height: 8 },
-		{ x: 3, y: -1, height: 4 },
+		{ x: 3, y: -9, width: 3, height: 12 },
+		{ x: 32, y: -9, height: 22, width: 9 },
+		{ x: 3, y: -9, width: 33, height: 8 },
 		{ x: 20, y: 6, height: 15, width: 5 },
-		{ x: 20, y: 6, height: 8, width: 15 },
+		{ x: 20, y: 6, height: 6, width: 15 },
 
 		// Shaft platforms
 		{ x: 15, y: 7, width: 2 },
@@ -19,7 +17,7 @@ var levelOne = {
 		{ x: 12, y: 23, width: 2 },
 		
 		// Lower Area
-		{ x: 11, y: 27, width: 45 },
+		{ x: 11, y: 27, width: 45, height: 8 },
 		{ x: 20, y: 20, width: 9 },
 		{ x: 28, y: 20, height: 4 },
 		{ x: 28, y: 23, width: 3 },
@@ -28,6 +26,7 @@ var levelOne = {
 		{ x: 55, y: 13, height: 4 }
 	],
 	mask: [
-		{ x: 9, y: 11, width: 2 }
+		{ x: 9, y: 11, width: 2 },
+		{ x: 33, y: 5, width: 3, height: 6 }
 	]
 }
